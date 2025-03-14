c76a2ad6eeb39c28bff602c3e4e071e310036e0d
goncxjo <gonzalo.moreira@live.com.ar>
2025-03-14 19:25:05 -0300

    se quita disponibilidad juego

--- src/app/backend/services/game.service.ts
+++ src/app/backend/services/game.service.ts
@@ -12,7 +12,7 @@ export class GameService {
     return [
       { id: 'digimon', name: 'Digimon Card Game', isAvailable: true, disabled: false },
       { id: 'one-piece', name: 'One Piece Card Game', isAvailable: true, disabled: false },
-      { id: 'dragon-ball-super', name: 'Dragon Ball Super', isAvailable: false, disabled: false },
+      { id: 'dragon-ball-super', name: 'Dragon Ball Super', isAvailable: false, disabled: true },
       { id: 'pokemon', name: 'Pokémon', isAvailable: false, disabled: true },
       { id: 'yu-gi-oh', name: 'Yu Gi Oh!', isAvailable: false, disabled: true },
       { id: 'magic', name: 'Magic: The Gathering', isAvailable: false, disabled: true }
